243a66ff636d19e9a87cb90182475978adfdfbe8
Sven337 <e5d0ceda@opayq.com>
2014-03-20 22:22:01 +0100

    edit.sh makes my life easier to edit a post

---
+++ edit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+FILE=$(find _drafts _posts -name '*.markdown' | sed -r 's,_(drafts|posts)/([0-9-]+)-(.*)\.markdown,\3\t\2\t\1\t\0,' | sort -rk 2 | sed 's/\t/\n/g' | yad --list --text="Posts" --column=Titre --column=Date --column=Type --column=file:HD  --print-column=4 --listen --separator='')
+
+if [ $? -eq 0 ]; then
+	vim $FILE
+fi
